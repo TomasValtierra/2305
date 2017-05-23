7fe728351405ddbf72cc7a8852da8d77aeedbcd5
cetecom <cetecom@LC08L5720>
2017-05-23 11:27:23 -0400

    control

---
+++ m/transaccion_agr.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+/*IDACCESO: Identificador del usuario
+
+ NOMTABLA: Nombre de la tabla a utilizar
+
+ ACCION: U[Update], D[Delete], I[Insert]
+
+ DESCRIPCION: Información desde donde se realizó el cambio, nombre página, y otros datos que usted
+
+defina.
+*/
